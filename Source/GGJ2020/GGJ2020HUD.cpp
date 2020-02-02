071543fccf2705dd54699e4d33da3693fc518b8f
AlexandreAubin <alexandre.aubin@hotmail.com>
2020-02-02 09:49:58 -0500

    Add a main menu

--- Source/GGJ2020/GGJ2020HUD.cpp
+++ Source/GGJ2020/GGJ2020HUD.cpp
@@ -6,6 +6,7 @@
 #include "TextureResource.h"
 #include "CanvasItem.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
 
 AGGJ2020HUD::AGGJ2020HUD()
 {
@@ -17,19 +18,27 @@ AGGJ2020HUD::AGGJ2020HUD()
 
 void AGGJ2020HUD::DrawHUD()
 {
-	Super::DrawHUD();
 
-	// Draw very simple crosshair
+	FString name = GetWorld()->GetName();
 
-	// find center of the Canvas
-	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+	if (name != TEXT("MainMenu"))
+	{
+		Super::DrawHUD();
 
-	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
-										   (Center.Y + 20.0f));
+		// Draw very simple crosshair
 
-	// draw the crosshair
-	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
-	TileItem.BlendMode = SE_BLEND_Translucent;
-	Canvas->DrawItem( TileItem );
+		// find center of the Canvas
+		const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+
+		// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
+		const FVector2D CrosshairDrawPosition((Center.X),
+			(Center.Y + 20.0f));
+
+		// draw the crosshair
+		FCanvasTileItem TileItem(CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+		TileItem.BlendMode = SE_BLEND_Translucent;
+		Canvas->DrawItem(TileItem);
+	}
+	
+
 }
